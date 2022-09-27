307878721cb569139a0333094d47d99b331614dc
agencik007 <agencik007@gmail.com>
2022-09-27 11:17:17 +0200

    Created shop entity

---
+++ src/shop/shop-item.entity.ts
@@ -0,0 +1,30 @@
+import { ItemInBasket } from "src/basket/item-in-basket.entity";
+import { ShopItemInterface } from "src/interfaces/shop";
+import { BaseEntity, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ShopItem extends BaseEntity implements ShopItemInterface {
+    @PrimaryGeneratedColumn('uuid') 
+    id: string
+
+    @Column({
+        length: 50,
+    })
+    name: string;
+
+    @Column({
+        length: 1000,
+    })
+    description: string;
+
+    @Column({
+        type: 'float', 
+        precision: 7,
+        scale: 2,
+    })
+    price: number;
+
+    @OneToOne(type => ItemInBasket, entity => entity.shopItem)
+    @JoinColumn()
+    itemInBasket: ItemInBasket;
+}
