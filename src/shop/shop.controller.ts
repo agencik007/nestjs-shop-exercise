5309458e48eb4d9c875d1dab6a247b1fdb7be9af
agencik007 <agencik007@gmail.com>
2022-09-27 11:20:33 +0200

    Created shop service and controller with necessary actions - get items, chceck if item exist, get price of one item, get one item

---
+++ src/shop/shop.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ShopItemInterface } from 'src/interfaces/shop';
+import { ShopService } from './shop.service';
+
+@Controller('shop')
+export class ShopController {
+    constructor(
+        @Inject(ShopService) private shopService: ShopService,
+    ){
+
+    }
+
+    @Get('/')
+    getShopList(): Promise<ShopItemInterface[]> {
+        return this.shopService.getItems();
+    }
+}
---
+++ src/shop/shop.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ShopItem } from './shop-item.entity';
+
+@Injectable()
+export class ShopService {
+    async getItems(): Promise<ShopItem[]> {
+        return await ShopItem.find();
+    }
+
+    async hasItem(name: string): Promise<boolean> {
+        return (await this.getItems()).some(item => item.name === name);
+    }
+
+    async getPrice(name: string): Promise<number> {
+        return (await this.getItems()).find(item => item.name === name).price;
+    }
+
+    async getOneItem(id: string): Promise<ShopItem> {
+        return await ShopItem.findOne({where: {id: id}})
+    }
+}
