2ceb2edb52509198cbe220bbac730a871fcaa110
agencik007 <agencik007@gmail.com>
2022-09-27 11:17:04 +0200

    Created entity for basket

---
+++ src/basket/item-in-basket.entity.ts
@@ -0,0 +1,17 @@
+import { ShopItem } from "src/shop/shop-item.entity";
+import { BaseEntity, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { ColumnMetadata } from "typeorm/metadata/ColumnMetadata";
+import { AddProductDto } from "./dto/add-product.dto";
+
+@Entity()
+export class ItemInBasket extends BaseEntity implements AddProductDto {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    count: number;
+
+    @OneToOne(type => ShopItem, entity => entity.itemInBasket)
+    @JoinColumn()
+    shopItem: ShopItem;
+}
