17e24bc8059a6e6e5582443c17f7b59c78d2dddf
agencik007 <agencik007@gmail.com>
2022-09-27 12:47:02 +0200

    Updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { BasketService } from './basket/basket.service';
 import { DatabaseModule } from './database/database.module';
 import { ShopController } from './shop/shop.controller';
 import { ShopService } from './shop/shop.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController, BasketController, ShopController],
-  providers: [AppService, BasketService, ShopService],
+  imports: [DatabaseModule, UserModule],
+  controllers: [AppController, BasketController, ShopController, UserController],
+  providers: [AppService, BasketService, ShopService, UserService],
 })
 export class AppModule {}
