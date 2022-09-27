f516637be908376891fe7ca5e6b1ca1f50093d07
agencik007 <agencik007@gmail.com>
2022-09-27 11:18:03 +0200

    Updated app module with services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BasketController } from './basket/basket.controller';
+import { BasketService } from './basket/basket.service';
 import { DatabaseModule } from './database/database.module';
+import { ShopController } from './shop/shop.controller';
+import { ShopService } from './shop/shop.service';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BasketController, ShopController],
+  providers: [AppService, BasketService, ShopService],
 })
 export class AppModule {}
