a38bf7c998cb64d4b09fd60193e6a1e53cee49a7
agencik007 <agencik007@gmail.com>
2022-10-15 19:04:13 +0200

    Password protect guard

--- src/guards/password-protect.guard.ts
+++ src/guards/password-protect.guard.ts
@@ -1,18 +1,17 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class PasswordProtectGuard implements CanActivate {
-    constructor(
-        private reflector: Reflector
-        ){}
-    
-        canActivate(
-        context: ExecutionContext,
-    ): boolean | Promise<boolean> {
-        const request = context.switchToHttp().getRequest();
-        const goodPass = this.reflector.get<string>('passwordProtectGoodPassword', context.getHandler());
+  constructor(private reflector: Reflector) {}
 
-        return request.headers['x-password'] === goodPass;
-    }
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const goodPass = this.reflector.get<string>(
+      'passwordProtectGoodPassword',
+      context.getHandler(),
+    );
+
+    return request.headers['x-password'] === goodPass;
+  }
 }
