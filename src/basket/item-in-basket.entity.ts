f5ff1f307450f579b6c8cb0cf411f5ad567076be
agencik007 <agencik007@gmail.com>
2022-10-15 18:21:07 +0200

    Updated shop item entity

--- src/basket/item-in-basket.entity.ts
+++ src/basket/item-in-basket.entity.ts
@@ -1,20 +1,27 @@
-import { ShopItem } from "src/shop/shop-item.entity";
-import { User } from "src/user/user.entity";
-import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { ShopItem } from 'src/shop/shop-item.entity';
+import { User } from 'src/user/user.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class ItemInBasket extends BaseEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    count: number;
+  @Column()
+  count: number;
 
-    @ManyToOne(type => ShopItem, entity => entity.itemsInBasket)
-    @JoinColumn()
-    shopItem: ShopItem;
+  @ManyToOne((type) => ShopItem, (entity) => entity.itemsInBasket)
+  @JoinColumn()
+  shopItem: ShopItem;
 
-    @ManyToOne(type =>User, entity => entity.itemsInBasket)
-    @JoinColumn()
-    user: User;
+  @ManyToOne((type) => User, (entity) => entity.itemsInBasket)
+  @JoinColumn()
+  user: User;
 }
